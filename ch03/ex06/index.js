331f6704cd61a068d8adfd5f7822d3f80836f402
SayakaYamaguchi <sayaka.yamaguchi@jp.ricoh.com>
2024-01-09 21:49:29 +0900

    ch03\ex06up

---
+++ ch03/ex06/index.js
@@ -0,0 +1,17 @@
+export function substring(str, indexStart, indexEnd) {
+  return str.substring(indexStart, indexEnd);
+}
+
+export function slice(str, indexStart, indexEnd) {
+  return str.slice(indexStart, indexEnd);
+}
+
+export function padStart(str, targetLength, padString) {
+  return str.padStart(targetLength, padString);
+}
+
+export function trim(str) {
+  return str.trim();
+}
+
+// module.exports = { mySubstring, mySlice, myPadStart, myTrim };
