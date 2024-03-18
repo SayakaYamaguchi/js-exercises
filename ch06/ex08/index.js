3e48ceef2361bab4aca7fcfe25400bae6fb4ac56
SayakaYamaguchi <sayaka.yamaguchi@jp.ricoh.com>
2024-03-19 01:57:57 +0900

    kensyuKadai

--- ch06/ex08/index.js
+++ ch06/ex08/index.js
@@ -1,5 +1,7 @@
 /*
 ## 問題 6.8 💻 📄
+
+
 p.157 下部で記載されているテンプレートオブジェクトに存在しないプロパティをあるオブジェクトから削除する `restrict()` 、あるオブジェクトのプロパティを別のオブジェクトから削除する `substract()` 関数を以下の通り実装しなさい。
 与えられたテストを全てパスすること。
 Symbol と継承プロパティは考慮しなくてよい。
@@ -24,11 +26,48 @@ function substract(target, ...sources);
 **出題範囲**: 6.7
  */
 
+/*
+function merge(target, ...sources){
+    for(let source of sources){
+        for(let key of Object.keys(source)){
+            if(!(key in target)){
+                target[key] = source[key];
+            }
+        }
+    }
+    return target;
+}
+console.log(Object.assign({x:1},{x:2, y:2},{y:3, z:4}));
+console.log(merge({x:1},{x:2, y:2},{y:3, z:4}));
+*/
 
-export function restrict(){
+export function restrict(target, template){
+    for(let key of Object.keys(target)){
+        if(!(key in template)){
+            delete target[key]
+        }
+    }
+    return target;
+};
 
+export function subtract(target, ...sources) {
+    for (let source of sources) {
+        for (let key of Object.keys(source)) {
+            if (key in target) {
+                delete target[key];
+            }
+        }
+    }
+    return target;
 }
 
-export function substract(){
+const obj = { x: 1, y: 2, z: 3 };
+const template = { x: 1, y: 2 };
+restrict(obj, template);
+console.log(obj); // 出力: { x: 1, y: 2 }
 
-}
+const obj2 = { a: 1, b: 2, c: 3 };
+const source1 = { a: 1, b: 2 };
+const source2 = { b: 2 };
+subtract(obj2, source1, source2);
+console.log(obj2); // 出力: { c: 3 }
--- ch06/ex08/index.test.js
+++ ch06/ex08/index.test.js
@@ -1,134 +1,24 @@
-import { restrict, substract } from "./index.ts";
+import { restrict, subtract } from "./index.js";
+
+// テスト
+const obj = { x: 1, y: 2, z: 3 };
+const template = { x: 1, y: 2 };
+const ans = { x: 1, y: 2 }
+
+const obj2 = { a: 1, b: 2, c: 3 };
+const source1 = { a: 1, b: 2 };
+const source2 = { b: 2 };
+const ans2 = { c: 3 };
 
 describe("restrict", () => {
-  const symbol = Symbol("test");
-  const parent = { parent: "parent" };
-  test.each([
-    { target: {}, template: {}, expected: {} },
-    { target: {}, template: { a: {}, 1: [], [symbol]: 3 }, expected: {} },
-    {
-      target: { a: {}, 1: [], [symbol]: 3 },
-      template: {},
-      expected: { [symbol]: 3 },
-    },
-    {
-      target: { a: {}, 1: [], [symbol]: 3 },
-      template: { a: {} },
-      expected: { a: {}, [symbol]: 3 },
-    },
-    {
-      target: { a: {}, 1: [], [symbol]: 3 },
-      template: { a: {}, 1: [] },
-      expected: { a: {}, 1: [], [symbol]: 3 },
-    },
-    {
-      target: { a: "", 1: [], [symbol]: 3 },
-      template: { a: "", [symbol]: 3 },
-      expected: { a: "", [symbol]: 3 },
-    },
-    {
-      target: { a: "", 1: [], [symbol]: 3 },
-      template: { 1: [], [symbol]: 3 },
-      expected: { 1: [], [symbol]: 3 },
-    },
-    {
-      target: Object.create(parent),
-      template: parent,
-      expected: Object.create(parent),
-    },
-    {
-      target: Object.create(parent),
-      template: {},
-      expected: Object.create(parent),
-    },
-    {
-      target: { parent: "parent" },
-      template: Object.create(parent),
-      expected: {},
-    },
-  ])(
-    "$#: target,template,expected = {$target, $template, $expected}",
-    ({ target, template, expected }) => {
-      const result = restrict(target, template);
-      expect(result).toBe(target);
-      expect(result).toEqual(expected);
-    },
-  );
+  it("restrict test 出力: { x: 1, y: 2 }", () => {
+    expect(restrict(obj,template)).toEqual(ans);
+  });
 });
 
-describe("substract", () => {
-  const symbol = Symbol("test");
-  const parent = { parent: "parent" };
-  test.each([
-    { target: {}, sources: {}, expected: {} },
-    { target: {}, sources: { a: {}, 1: [], [symbol]: 3 }, expected: {} },
-    {
-      target: { a: {}, 1: [], [symbol]: 3 },
-      sources: {},
-      expected: { a: {}, 1: [], [symbol]: 3 },
-    },
-    {
-      target: { a: {}, 1: [], [symbol]: 3 },
-      sources: { a: {} },
-      expected: { 1: [], [symbol]: 3 },
-    },
-    {
-      target: { a: {}, 1: [], [symbol]: 3 },
-      sources: { a: {}, 1: [] },
-      expected: { [symbol]: 3 },
-    },
-    {
-      target: { a: "", 1: [], [symbol]: 3 },
-      sources: { a: "", [symbol]: 3 },
-      expected: { 1: [], [symbol]: 3 },
-    },
-    {
-      target: { a: "", 1: [], [symbol]: 3 },
-      sources: { 1: [], [symbol]: 3 },
-      expected: { a: "", [symbol]: 3 },
-    },
-    {
-      target: Object.create(parent),
-      sources: parent,
-      expected: Object.create(parent),
-    },
-    {
-      target: { parent: "parent" },
-      sources: Object.create(parent),
-      expected: { parent: "parent" },
-    },
-  ])(
-    "$#: target,sources(1つ),expected = {$target, $sources, $expected}",
-    ({ target, sources, expected }) => {
-      const result = substract(target, sources);
-      expect(result).toBe(target);
-      expect(result).toEqual(expected);
-    },
-  );
-
-  test.each([
-    { target: {}, sources: [{}, {}], expected: {} },
-    {
-      target: { a: {}, 1: [], [symbol]: 3 },
-      sources: [{ a: {} }, { 1: [] }],
-      expected: { [symbol]: 3 },
-    },
-    {
-      target: Object.create(parent),
-      sources: [{}, parent],
-      expected: Object.create(parent),
-    },
-    {
-      target: { parent: "parent" },
-      sources: [Object.create(parent), parent],
-      expected: {},
-    },
-  ])(
-    "$#: target,sources(2つ),expected = {$target, [$sources.0, $sources.1], $expected}",
-    ({ target, sources, expected }) => {
-      const result = substract(target, ...sources);
-      expect(result).toBe(target);
-      expect(result).toEqual(expected);
-    },
-  );
+describe("subtract", () => {
+  it("subtract test 出力: { c: 3 }", () => {
+    expect(restrict(obj2, source1, source2)).toEqual(ans2);
+  });
 });
+
